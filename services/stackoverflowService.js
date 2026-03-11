c5f6041c947a611a7c67b0fc2ffa44cde80cd979
Mihir Bommisetty <38711080+munnamihir@users.noreply.github.com>
2026-03-11 19:34:20 -0400

    Update stackoverflowService.js

--- services/stackoverflowService.js
+++ services/stackoverflowService.js
@@ -2,16 +2,23 @@ import axios from "axios";
 
 export async function searchStackOverflow(query) {
 
-  const url = "https://api.stackexchange.com/2.3/search/advanced";
+  const url = `https://api.stackexchange.com/2.3/search/advanced`;
 
   const response = await axios.get(url, {
     params: {
       order: "desc",
       sort: "relevance",
       q: query,
-      site: "stackoverflow"
+      site: "stackoverflow",
+      accepted: true,
+      pagesize: 5
     }
   });
 
-  return response.data.items.slice(0, 5);
+  return response.data.items.map(item => ({
+    title: item.title,
+    link: item.link,
+    score: item.score
+  }));
+
 }
